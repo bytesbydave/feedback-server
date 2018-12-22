5db04cb64aec6a4049c52e5be27690060197e1c1
Dave <dave.lin21@gmail.com>
2018-12-22 17:59:38 -0500

    Routing with Proxy

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Hi</p>
+          <a href="/auth/google">Sign In with Google</a>
         </header>
       </div>
     );
