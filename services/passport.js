182236494290ea2150ae935f95829c5aa850925f
Dave <dave.lin21@gmail.com>
2018-12-22 19:23:08 -0500

    refactor passport to async awaits

--- services/passport.js
+++ services/passport.js
@@ -24,20 +24,34 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({
-            googleId: profile.id,
-            firstName: profile.name.givenName,
-            lastName: profile.name.familyName
-          })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({
+        googleId: profile.id,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName
+      }).save();
+      done(null, user);
     }
+    // (accessToken, refreshToken, profile, done) => {
+    //   User.findOne({ googleId: profile.id })
+    //     .then(existingUser => {
+    //     if (existingUser) {
+    //       done(null, existingUser);
+    //     } else {
+    //       new User({
+    //         googleId: profile.id,
+    //         firstName: profile.name.givenName,
+    //         lastName: profile.name.familyName
+    //       })
+    //         .save()
+    //         .then(user => done(null, user));
+    //     }
+    //   });
+    // }
   )
 );
