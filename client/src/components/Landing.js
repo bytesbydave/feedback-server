6338d1fd6e81f2183a10e847cc6c37dde3685ae2
Dave <dave.lin21@gmail.com>
2018-12-23 05:01:58 -0500

    create landing page component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>Feedback</h1>
+      <h5>Generate Customer Feedbacks</h5>
+    </div>
+  );
+};
+
+export default Landing;
