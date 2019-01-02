63f92ad15724def39c8facfb33ff365342f61650
Dave <dave.lin21@gmail.com>
2019-01-02 01:09:58 -0500

    display surveys on dashboard

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     return (
       <div className="container">
         <BrowserRouter>
-          <div className="container">
+          <div>
             <Header />
             <Route path="/" exact component={Landing} />
             <Route path="/surveys" exact component={Dashboard} />
